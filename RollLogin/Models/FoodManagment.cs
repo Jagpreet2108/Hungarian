5d3471c6fdc0821e6df423ffcf0bf2dd8e2e0e5d
somnathchaudhary <somuchaudhary0506@gmail.com>
2020-04-06 11:44:37 +0530

    Somnath Food Managment

---
+++ RollLogin/Models/FoodManagment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RollLogin.Models
+{
+    [Table("FoodMag")]
+    public class FoodManagment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string FId { get; set; }
+        [Key]
+        public string Fcode { get; set; }
+        public string FName { get; set; }
+        public float FPrice { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
