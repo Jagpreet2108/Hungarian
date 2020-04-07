bf31aa4a9b591e064d380a4695a80ea622591664
ketansharma97 <97.ketansharma@gmail.com>
2020-04-07 16:26:30 +0530

    home index

---
+++ RollLogin/Data/Migrations/20200407103727_Superman.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RollLogin.Data.Migrations
+{
+    public partial class Superman : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FoodMag",
+                columns: table => new
+                {
+                    FId = table.Column<string>(nullable: false),
+                    Fcode = table.Column<string>(nullable: true),
+                    FName = table.Column<string>(nullable: true),
+                    FPrice = table.Column<float>(nullable: false),
+                    FDisc = table.Column<string>(nullable: true),
+                    Quantity = table.Column<int>(nullable: false),
+                    Atri = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    ODate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodMag", x => x.FId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FoodMag");
+        }
+    }
+}
